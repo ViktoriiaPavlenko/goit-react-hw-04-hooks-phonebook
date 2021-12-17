d8c4d1a4f194d2c50ceeabec322128e91661595c
ViktoriiaPavlenko <vktr9999@ukr.net>
2021-12-17 11:16:54 +0200

    corrected locale storage

--- src/hooks/useLocaleStorage.jsx
+++ src/hooks/useLocaleStorage.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
 
-export default function useLocaleStorage(key, defaultValue) {
-  const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-  });
-
+const getValue = (key, initValue) => {
+  const value = localStorage.getItem(key);
+  if (value) {
+    try {
+      return JSON.parse(value);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return initValue;
+};
+export default function useLocaleStorage(key, initValue) {
+  const [state, setState] = useState(getValue(key, initValue));
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [state, key]);
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
   return [state, setState];
 }
